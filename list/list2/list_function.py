7a38a65d40d8421b1ac1c4724334d9640ae76cbb
tresordie <tresordie@163.com>
2021-02-10 04:34:20 +0800

    insert function

--- list/list2/list_function.py
+++ list/list2/list_function.py
@@ -1,9 +1,10 @@
 # python的列表有哪些方法
 # 1.列表末尾增加一个数据项             append
 # 2.列表末尾删除数据                  pop
-# 3.列表末尾增加一个数据项集合          extend
+# 3.列表末尾增加一个数据项集合          extend(其将extend中的数据项集合中的数据拼接到列表的后面)
 # 4.列表中找到并删除一个特定的数据项     remove
 # 5.列表中的长度                     len
+# 6.列表中某个特定位置前增加一个数据项   insert
 
 
 movies = ['The Holy Grail', "The Life of Brian", 'The Meaning of Life']
@@ -21,4 +22,7 @@ print(movies)
 print(len(movies))
 
 movies.remove("The Holy Grail")
+print(movies)
+
+movies.insert(0, "FuckU")
 print(movies)
